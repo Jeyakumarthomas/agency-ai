029cf3299342ea7f36fb560cd8b9c7a0ebbfa137
Jeyakumarthomas <jeyakumarthomas23123@example.com>
2025-12-16 09:19:13 +0530

    Full website on view animation loaded

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Title from './Title.jsx'
 import assets from '../assets/assets';
 import ServiceCard from './ServiceCard.jsx';
+import { motion } from "motion/react"
 
 const Services = () => {
     const servicesData = [
@@ -24,7 +25,13 @@ const Services = () => {
         }
     ]
   return (
-        <div id="services" className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24
+        <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once:true}}
+        transition={{staggerChildren:0.2}}
+        
+        id="services" className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24
         xl:px-40 pt-30 text-gray-700 dark:text-white' > 
 
         <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden'/>
@@ -37,7 +44,7 @@ const Services = () => {
             ))}
         </div>
 
-        </div>
+        </motion.div>
   )
 }
 
